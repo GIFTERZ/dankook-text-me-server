dde3a1bb998b3178d2b84f42e8684469c7fa4bc5
juy4556 <wnstjrtmdgus@gmail.com>
2024-02-27 16:56:39 +0900

    refactor: pkce 추가로 getAuthCodeRequestUrl, login로직 변경

--- src/main/java/gifterz/textme/domain/oauth/service/OauthService.java
+++ src/main/java/gifterz/textme/domain/oauth/service/OauthService.java
@@ -1,8 +1,9 @@
 package gifterz.textme.domain.oauth.service;
 
-import gifterz.textme.domain.oauth.dto.OauthRequest;
+import gifterz.textme.domain.oauth.dto.OauthLoginRequest;
 import gifterz.textme.domain.oauth.entity.OauthId;
 import gifterz.textme.domain.oauth.entity.OauthMember;
+import gifterz.textme.domain.oauth.infra.dku.dto.AuthCodeRequest;
 import gifterz.textme.domain.oauth.util.AuthCodeRequestUrlProviderMapper;
 import gifterz.textme.domain.oauth.entity.AuthType;
 import gifterz.textme.domain.oauth.util.OauthMemberClientMapper;
@@ -15,11 +16,12 @@ import gifterz.textme.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import gifterz.textme.domain.oauth.repository.OauthMemberRepository;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OauthService {
 
     private final AuthCodeRequestUrlProviderMapper authCodeRequestUrlProviderMapper;
@@ -29,15 +31,29 @@ public class OauthService {
     private final AesUtils aesUtils;
     private final RefreshTokenService refreshTokenService;
 
-    public String getAuthCodeRequestUrl(AuthType authType) {
-        return authCodeRequestUrlProviderMapper.provide(authType);
+    public String getAuthCodeRequestUrl(AuthCodeRequest authCodeRequest) {
+        return authCodeRequestUrlProviderMapper.provide(authCodeRequest.getAuthType(), authCodeRequest.toPkceRequest());
     }
 
-    public LoginResponse login(AuthType authType, OauthRequest oauthRequest) {
-        OauthMember oauthMember = oauthMemberClientMapper.fetch(authType, oauthRequest.authCode());
+    @Transactional
+    public LoginResponse login(AuthType authType, OauthLoginRequest oauthLoginRequest) {
+        String authCode = oauthLoginRequest.authCode();
+        String codeVerifier = oauthLoginRequest.codeVerifier();
+        if (codeVerifier != null) {
+            return loginWithPkce(authType, authCode, codeVerifier);
+        }
+        OauthMember oauthMember = oauthMemberClientMapper.fetch(authType, authCode);
+        return getLoginResponse(oauthMember);
+    }
+
+    private LoginResponse loginWithPkce(AuthType authType, String authCode, String codeVerifier) {
+        OauthMember oauthMember = oauthMemberClientMapper.fetch(authType, authCode, codeVerifier);
+        return getLoginResponse(oauthMember);
+    }
+
+    private LoginResponse getLoginResponse(OauthMember oauthMember) {
         OauthId oauthId = oauthMember.getOauthId();
-        Optional<OauthMember> oauthMemberOptional = oauthMemberRepository.findByOauthId(oauthId);
-        if (oauthMemberOptional.isEmpty()) {
+        if (!oauthMemberRepository.existsByOauthId(oauthId)) {
             oauthMemberRepository.save(oauthMember);
         }
         User user = oauthMember.getUser();
