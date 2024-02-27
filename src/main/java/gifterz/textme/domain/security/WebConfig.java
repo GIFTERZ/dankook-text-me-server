a28dff927c8005baf3a53599a0a830b9fd4b4db1
juy4556 <wnstjrtmdgus@gmail.com>
2024-02-27 16:37:55 +0900

    feat: snakeCase로 받은 param camelCase로 맵핑하도록 변환 필터 추가

--- src/main/java/gifterz/textme/domain/security/WebConfig.java
+++ src/main/java/gifterz/textme/domain/security/WebConfig.java
@@ -1,15 +1,28 @@
 package gifterz.textme.domain.security;
 
+import com.google.common.base.CaseFormat;
 import gifterz.textme.domain.oauth.util.OauthServerTypeConverter;
 import gifterz.textme.domain.security.jwt.JwtAuthArgumentResolver;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @RequiredArgsConstructor
 @Configuration
@@ -34,5 +47,48 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new OauthServerTypeConverter());
+    }
+
+    @Bean
+    public OncePerRequestFilter snakeCaseConverterFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+                final Map<String, String[]> parameters = new ConcurrentHashMap<>();
+
+                for (String param : request.getParameterMap().keySet()) {
+                    String camelCaseParam = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, param);
+
+                    parameters.put(camelCaseParam, request.getParameterValues(param));
+                    parameters.put(param, request.getParameterValues(param));
+                }
+
+                filterChain.doFilter(new HttpServletRequestWrapper(request) {
+
+                    @Override
+                    public String getParameter(String name) {
+                        return parameters.containsKey(name) ? parameters.get(name)[0] : null;
+                    }
+
+                    @Override
+                    public Enumeration<String> getParameterNames() {
+                        return Collections.enumeration(parameters.keySet());
+                    }
+
+                    @Override
+                    public String[] getParameterValues(String name) {
+                        return parameters.get(name);
+                    }
+
+                    @Override
+                    public Map<String, String[]> getParameterMap() {
+                        return parameters;
+                    }
+
+                }, response);
+            }
+
+        };
     }
 }
