409a6b8f6b2d2f8c38baa39b0e99bba3999dcd4f
juy4556 <wnstjrtmdgus@gmail.com>
2024-02-28 23:12:12 +0900

    refactor: dku 멤버 페치 리팩토링

--- src/main/java/gifterz/textme/domain/oauth/infra/dku/service/DkuMemberClient.java
+++ src/main/java/gifterz/textme/domain/oauth/infra/dku/service/DkuMemberClient.java
@@ -7,8 +7,8 @@ import gifterz.textme.domain.oauth.entity.OauthMemberClient;
 import gifterz.textme.domain.oauth.infra.dku.config.DkuOauthConfig;
 import gifterz.textme.domain.oauth.infra.dku.controller.DkuApi;
 import gifterz.textme.domain.oauth.infra.dku.dto.DkuMemberResponse;
-import gifterz.textme.domain.oauth.infra.dku.dto.MajorInfo;
 import gifterz.textme.domain.oauth.infra.dku.entity.DkuTokenResponse;
+import gifterz.textme.domain.oauth.infra.dku.vo.DkuStudentInfo;
 import gifterz.textme.domain.user.entity.Major;
 import gifterz.textme.domain.user.entity.User;
 import gifterz.textme.domain.user.repository.MajorRepository;
@@ -36,21 +36,29 @@ public class DkuMemberClient implements OauthMemberClient {
     public OauthMember fetchMember(String authCode, String... params) {
         DkuTokenResponse dkuTokenResponse = fetchDkuToken(authCode, params[0]);
         String scope = dkuTokenResponse.scope();
-
         DkuMemberResponse response = dkuApi.fetchMemberInfo(dkuTokenResponse.createToken(), scope);
-        OauthId oauthId = OauthId.of(response.userId(), AuthType.DKU);
-        String email = response.studentIdToEmail();
-        String name = response.name();
-        MajorInfo majorInfo = response.major();
-        Major major = Major.of(majorInfo.department(), majorInfo.name());
-        if (!majorRepository.existsByDepartmentAndName(major.getDepartment(), major.getName())) {
-            majorRepository.save(major);
-        }
+        DkuStudentInfo dkuStudentInfo = response.toDkuStudentInfo();
+        OauthId oauthId = OauthId.of(dkuStudentInfo.getUserId(), AuthType.DKU);
+        String email = dkuStudentInfo.getEmail();
+        String name = dkuStudentInfo.getUsername();
+        Major major = getMajor(dkuStudentInfo);
         User user = getUser(email, name, major);
+        checkIsPasswordType(user);
+        return OauthMember.of(user, oauthId);
+    }
+
+    private static void checkIsPasswordType(User user) {
         if (user.getAuthType() == AuthType.PASSWORD) {
             user.updateAuthType(AuthType.DKU);
         }
-        return OauthMember.of(user, oauthId);
+    }
+
+    private Major getMajor(DkuStudentInfo dkuStudentInfo) {
+        Major major = dkuStudentInfo.toMajor();
+        if (!majorRepository.existsByDepartmentAndName(major.getDepartment(), major.getName())) {
+            majorRepository.save(major);
+        }
+        return major;
     }
 
     private User getUser(String email, String name, Major major) {
