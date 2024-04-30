da3756895604c44c48bb474cdeb19e089155b62d
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-01 00:27:43 +0900

    feature: 로컬 캐시로 caffeine 추가

--- src/main/java/gifterz/textme/global/cache/CacheConfig.java
+++ src/main/java/gifterz/textme/global/cache/CacheConfig.java
@@ -1,34 +1,30 @@
-package gifterz.textme.config;
+package gifterz.textme.global.cache;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import gifterz.textme.common.firebase.CacheProperties;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 
 @Configuration
 @EnableCaching
-@RequiredArgsConstructor
 public class CacheConfig {
     @Bean
     public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
@@ -38,17 +34,29 @@ public class CacheConfig {
                 .build();
     }
 
-    private final CacheProperties cacheProperties;
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
 
-    @Value("${spring.redis.cache.host}")
-    private String redisHost;
+        List<CaffeineCache> caches = Arrays.stream(LocalCacheType.values())
+                .map(createCaffeineCache())
+                .toList();
 
-    @Value("${spring.redis.cache.port}")
-    private int redisPort;
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
 
-    @Bean(name = "redisCacheConnectionFactory")
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+    private Function<LocalCacheType, CaffeineCache> createCaffeineCache() {
+        return cache -> new CaffeineCache(
+                cache.getName(),
+                Caffeine.newBuilder()
+                        .expireAfterWrite(cache.getExpireTime(), cache.getTimeUnit())
+                        .maximumSize(cache.getMaximumSize())
+                        .recordStats()
+                        .build()
+        );
+    }
+
     private RedisCacheConfiguration redisCacheDefaultConfiguration() {
         return RedisCacheConfiguration
                 .defaultCacheConfig()
@@ -59,7 +67,6 @@ public class CacheConfig {
     }
 
     private ObjectMapper objectMapper() {
-
         PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator
                 .builder()
                 .allowIfSubType(Object.class)
@@ -69,33 +76,5 @@ public class CacheConfig {
         mapper.registerModule(new JavaTimeModule());
         mapper.activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL);
         return mapper;
-    }
-
-    private RedisCacheConfiguration redisCacheDefaultConfiguration() {
-        return RedisCacheConfiguration
-                .defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper())));
-    }
-
-    private Map<String, RedisCacheConfiguration> redisCacheConfigurationMap() {
-        Map<String, RedisCacheConfiguration> cacheConfigurationMap = new HashMap<>();
-        for (Entry<String, Long> cacheNameAndTimeout : cacheProperties.getTtl().entrySet()) {
-            cacheConfigurationMap
-                    .put(cacheNameAndTimeout.getKey(), redisCacheDefaultConfiguration().entryTtl(
-                            Duration.ofSeconds(cacheNameAndTimeout.getValue())));
-        }
-        return cacheConfigurationMap;
-    }
-
-    @Bean
-    public CacheManager redisCacheManager(
-            @Qualifier("redisCacheConnectionFactory") RedisConnectionFactory redisConnectionFactory) {
-        return RedisCacheManager.RedisCacheManagerBuilder
-                .fromConnectionFactory(redisConnectionFactory)
-                .cacheDefaults(redisCacheDefaultConfiguration())
-                .withInitialCacheConfigurations(redisCacheConfigurationMap())
-                .build();
     }
 }
