124c85bd73c09b4a110d58e1b4726637ba67ff9b
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-01 00:35:56 +0900

    feature: card 업로드 및 가져오기 로직에 로컬 캐시 적용

--- src/main/java/gifterz/textme/domain/card/service/CardService.java
+++ src/main/java/gifterz/textme/domain/card/service/CardService.java
@@ -4,6 +4,8 @@ import gifterz.textme.domain.card.entity.Card;
 import gifterz.textme.domain.card.repository.CardRepository;
 import gifterz.textme.s3Proxy.S3Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +22,7 @@ public class CardService {
     private final S3Service s3Service;
 
     @Transactional
+    @CacheEvict(cacheNames = "card", allEntries = true, cacheManager = "caffeineCacheManager")
     public String uploadCard(MultipartFile multipartFile) throws IOException {
         String uploadUrl = s3Service.upload(multipartFile);
         Card card = Card.from(uploadUrl);
@@ -27,6 +30,7 @@ public class CardService {
         return card.getImageUrl();
     }
 
+    @Cacheable(cacheNames = "card", cacheManager = "caffeineCacheManager")
     public List<String> findCards() {
         return cardRepository.findAll()
                 .stream()
