abbe1584818c04051f7e829933389ca758c154d0
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-10 16:04:38 +0900

    feature: findByIdWithOptimistic dao 메서드 추가

--- src/main/java/gifterz/textme/domain/letter/repository/EventLetterRepository.java
+++ src/main/java/gifterz/textme/domain/letter/repository/EventLetterRepository.java
@@ -1,9 +1,16 @@
 package gifterz.textme.domain.letter.repository;
 
 import gifterz.textme.domain.letter.entity.EventLetter;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 public interface EventLetterRepository extends JpaRepository<EventLetter, Long> {
+
+    @Lock(value = LockModeType.OPTIMISTIC)
+    Optional<EventLetter> findByIdWithOptimistic(Long id);
 }
