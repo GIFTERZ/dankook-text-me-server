69469488e494aa5cc91075835611a9a2fa571cee
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-10 16:05:32 +0900

    feature: findLetter 서비스 메서드 추가

--- src/main/java/gifterz/textme/domain/letter/service/EventLetterService.java
+++ src/main/java/gifterz/textme/domain/letter/service/EventLetterService.java
@@ -2,20 +2,30 @@ package gifterz.textme.domain.letter.service;
 
 import gifterz.textme.domain.letter.dto.request.SenderInfo;
 import gifterz.textme.domain.letter.dto.request.Target;
+import gifterz.textme.domain.letter.dto.response.EventLetterResponse;
 import gifterz.textme.domain.letter.entity.EventLetter;
+import gifterz.textme.domain.letter.entity.EventLetterLog;
+import gifterz.textme.domain.letter.exception.LetterNotFoundException;
+import gifterz.textme.domain.letter.repository.EventLetterLogRepository;
 import gifterz.textme.domain.letter.repository.EventLetterRepository;
 import gifterz.textme.domain.user.entity.User;
 import gifterz.textme.domain.user.exception.UserNotFoundException;
 import gifterz.textme.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class EventLetterService {
     private final EventLetterRepository eventLetterRepository;
+    private final EventLetterLogRepository eventLetterLogRepository;
     private final UserRepository userRepository;
 
     @Transactional
@@ -24,5 +34,41 @@ public class EventLetterService {
         EventLetter eventLetter = EventLetter.of(user, senderInfo.getSenderName(), letterInfo.getContents(),
                 letterInfo.getImageUrl(), senderInfo.getContactInfo());
         eventLetterRepository.save(eventLetter);
+    }
+
+    @Transactional
+    public synchronized EventLetterResponse findLetter(Long userId, Long letterId) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        long userViewCount = eventLetterLogRepository.countByUser(user);
+        checkUserViewCount(userViewCount);
+        EventLetter eventLetter = getEventLetterByIdWithOptimistic(letterId).orElseThrow(LetterNotFoundException::new);
+        checkLetterViewCount(eventLetter.getViewCount());
+        eventLetter.increaseViewCount();
+        EventLetterLog eventLetterLog = EventLetterLog.of(user, eventLetter);
+        eventLetterLogRepository.save(eventLetterLog);
+        return EventLetterResponse.of(eventLetter);
+    }
+
+    private Optional<EventLetter> getEventLetterByIdWithOptimistic(Long letterId) {
+        while (true) {
+            try {
+                return eventLetterRepository.findByIdWithOptimistic(letterId);
+            } catch (ObjectOptimisticLockingFailureException e) {
+                log.info("현재 쓰레드: {}, letterId: {}", Thread.currentThread().getId(), letterId);
+            }
+        }
+
+    }
+
+    private void checkUserViewCount(long viewCount) {
+        if (viewCount >= 3) {
+            throw new IllegalArgumentException("이미 3번 조회한 사용자입니다.");
+        }
+    }
+
+    private void checkLetterViewCount(long viewCount) {
+        if (viewCount >= 3) {
+            throw new IllegalArgumentException("이미 3번 조회된 편지입니다.");
+        }
     }
 }
