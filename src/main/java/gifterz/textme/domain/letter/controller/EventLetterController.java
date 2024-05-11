2e371d0e76ad0920725c2069f4f7e6cc83a15269
ji-mango <susie000301@naver.com>
2024-05-11 22:09:09 +0900

    feature: EventLetter 컨트롤러 내에 getLetterByGender 메서드 추가

--- src/main/java/gifterz/textme/domain/letter/controller/EventLetterController.java
+++ src/main/java/gifterz/textme/domain/letter/controller/EventLetterController.java
@@ -1,6 +1,7 @@
 package gifterz.textme.domain.letter.controller;
 
 import gifterz.textme.domain.letter.dto.request.EventLetterRequest;
+import gifterz.textme.domain.letter.dto.response.AllEventLetterResponse;
 import gifterz.textme.domain.letter.service.EventLetterService;
 import gifterz.textme.global.auth.role.UserAuth;
 import gifterz.textme.global.security.jwt.JwtAuthentication;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/letters/events")
@@ -21,6 +24,12 @@ public class EventLetterController {
     public ResponseEntity<Void> sendLetter(JwtAuthentication auth, @RequestBody @Valid EventLetterRequest request) {
         eventLetterService.sendLetter(auth.getUserId(), request.toSenderInfo(), request.toTarget());
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AllEventLetterResponse>> getLettersByGender(@RequestParam String gender) {
+        List<AllEventLetterResponse> letterResponses = eventLetterService.getLettersByGender(gender);
+        return ResponseEntity.ok().body(letterResponses);
     }
 
 }
