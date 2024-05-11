aabc81a738b6bdc9c96384221a7522c5f8a742cf
ji-mango <susie000301@naver.com>
2024-05-11 22:10:05 +0900

    feature: EventLetter 서비스 내에 getLettersByGender 메서드 추가

--- src/main/java/gifterz/textme/domain/letter/service/EventLetterService.java
+++ src/main/java/gifterz/textme/domain/letter/service/EventLetterService.java
@@ -1,7 +1,9 @@
 package gifterz.textme.domain.letter.service;
 
+import gifterz.textme.domain.entity.StatusType;
 import gifterz.textme.domain.letter.dto.request.SenderInfo;
 import gifterz.textme.domain.letter.dto.request.Target;
+import gifterz.textme.domain.letter.dto.response.AllEventLetterResponse;
 import gifterz.textme.domain.letter.entity.EventLetter;
 import gifterz.textme.domain.letter.repository.EventLetterRepository;
 import gifterz.textme.domain.user.entity.User;
@@ -10,6 +12,9 @@ import gifterz.textme.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -24,5 +29,21 @@ public class EventLetterService {
         EventLetter eventLetter = EventLetter.of(user, senderInfo.getSenderName(), letterInfo.getContents(),
                 letterInfo.getImageUrl(), senderInfo.getContactInfo());
         eventLetterRepository.save(eventLetter);
+    }
+
+    public List<AllEventLetterResponse> getLettersByGender(String gender) {
+        if (gender.equals("men")) {
+            gender = "남자";
+        } else if (gender.equals("women")) {
+            gender = "여자";
+        }
+
+        List<EventLetter> eventLettersByGender = eventLetterRepository.findAllByUserGenderAndStatus(gender, StatusType.ACTIVATE.getStatus());
+        return eventLettersByGender.stream()
+                .map(eventLetter -> AllEventLetterResponse.builder()
+                        .id(eventLetter.getId())
+                        .imageUrl(eventLetter.getImageUrl())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
