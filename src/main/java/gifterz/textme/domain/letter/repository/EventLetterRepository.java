5a00b8c2968bb7f4251ebe38fd9bc16df0921e88
ji-mango <susie000301@naver.com>
2024-05-11 22:10:50 +0900

    feature: EventLetter 레포지토리 내에 findAllByUserGenderAndStatus 메서드 추가

--- src/main/java/gifterz/textme/domain/letter/repository/EventLetterRepository.java
+++ src/main/java/gifterz/textme/domain/letter/repository/EventLetterRepository.java
@@ -4,6 +4,9 @@ import gifterz.textme.domain.letter.entity.EventLetter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Transactional(readOnly = true)
 public interface EventLetterRepository extends JpaRepository<EventLetter, Long> {
+    List<EventLetter> findAllByUserGenderAndStatus(String gender, String status);
 }
