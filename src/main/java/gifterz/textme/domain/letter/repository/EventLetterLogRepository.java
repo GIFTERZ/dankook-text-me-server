b83733f9fc95919c4ff72238d6a83b2076a4439c
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-19 23:51:33 +0900

    feature: EventLetterLogRepository 내 List<EventLetterLog> 반환하는 findAllByUserId 메서드 추가

--- src/main/java/gifterz/textme/domain/letter/repository/EventLetterLogRepository.java
+++ src/main/java/gifterz/textme/domain/letter/repository/EventLetterLogRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 
 @Transactional(readOnly = true)
 public interface EventLetterLogRepository extends JpaRepository<EventLetterLog, Long> {
 
     @Query("select count(e) from EventLetterLog e where e.user = ?1")
     long countByUser(User user);
+
+    List<EventLetterLog> findAllByUserId(Long userId);
 }
