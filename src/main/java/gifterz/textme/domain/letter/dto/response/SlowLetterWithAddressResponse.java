9e1da783a451392706bb97658b00a34fe3e19e79
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-19 23:09:21 +0900

    refactor: SlowLetterWithAddressResponse 내 불필요한 import(Address) 제거

--- src/main/java/gifterz/textme/domain/letter/dto/response/SlowLetterWithAddressResponse.java
+++ src/main/java/gifterz/textme/domain/letter/dto/response/SlowLetterWithAddressResponse.java
@@ -2,7 +2,6 @@ package gifterz.textme.domain.letter.dto.response;
 
 import gifterz.textme.domain.letter.dto.request.ReceiverInfo;
 import gifterz.textme.domain.letter.dto.request.SenderInfo;
-import gifterz.textme.domain.letter.entity.Address;
 
 public record SlowLetterWithAddressResponse(
         SenderInfo senderInfo,
