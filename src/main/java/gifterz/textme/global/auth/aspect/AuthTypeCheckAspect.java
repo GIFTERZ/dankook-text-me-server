2d3a98e7d78cb1f6cd6ef4b6d2db258b7ca185da
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-20 22:49:25 +0900

    feature: Dku 소셜로그인인지 확인하는 Aspect 추가

---
+++ src/main/java/gifterz/textme/global/auth/aspect/AuthTypeCheckAspect.java
@@ -0,0 +1,33 @@
+package gifterz.textme.global.auth.aspect;
+
+import gifterz.textme.domain.oauth.entity.AuthType;
+import gifterz.textme.error.exception.NoAuthorizationException;
+import gifterz.textme.global.auth.role.UserRole;
+import gifterz.textme.global.security.jwt.JwtAuthentication;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AuthTypeCheckAspect {
+
+    @Before("gifterz.textme.global.auth.pointcut.Pointcuts.dkuAuth()")
+    public void checkDkuAuth(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof JwtAuthentication authentication) {
+                if (hasDkuAuth(authentication.getUserRole(), authentication.getAuthType())) return;
+                throw new NoAuthorizationException("DKU 로그인 시에만 접근 가능합니다.");
+            }
+        }
+    }
+
+    private static boolean hasDkuAuth(UserRole userRole, AuthType authType) {
+        if (userRole == UserRole.USER || userRole == UserRole.ADMIN) {
+            return authType == AuthType.DKU;
+        }
+        return false;
+    }
+}
