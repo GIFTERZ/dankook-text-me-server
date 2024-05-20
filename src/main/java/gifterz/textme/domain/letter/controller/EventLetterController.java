12ff1bd777269127f574eca23688752913dd0fce
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-20 22:50:34 +0900

    refactor: 이벤트 편지 관련 API에 대해 사용자 권한이 있고, AuthType이 DKU인지 확인하도록 DkuAuth 어노테이션 추가

--- src/main/java/gifterz/textme/domain/letter/controller/EventLetterController.java
+++ src/main/java/gifterz/textme/domain/letter/controller/EventLetterController.java
@@ -5,8 +5,7 @@ import gifterz.textme.domain.letter.dto.response.AdminEventLetterResponse;
 import gifterz.textme.domain.letter.dto.response.AllEventLetterResponse;
 import gifterz.textme.domain.letter.dto.response.EventLetterResponse;
 import gifterz.textme.domain.letter.service.EventLetterService;
-import gifterz.textme.global.auth.role.AdminAuth;
-import gifterz.textme.global.auth.role.UserAuth;
+import gifterz.textme.global.auth.role.DkuAuth;
 import gifterz.textme.global.security.jwt.JwtAuthentication;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,14 +22,14 @@ public class EventLetterController {
     private final EventLetterService eventLetterService;
 
     @PostMapping
-    @UserAuth
+    @DkuAuth
     public ResponseEntity<Void> sendLetter(JwtAuthentication auth, @RequestBody @Valid EventLetterRequest request) {
         eventLetterService.sendLetter(auth.getUserId(), request.toSenderInfo(), request.toTarget());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping
-    @UserAuth
+    @DkuAuth
     public ResponseEntity<List<AllEventLetterResponse>> getLettersByGender(
             JwtAuthentication auth,
             @RequestParam(required = false) String gender
@@ -40,7 +39,7 @@ public class EventLetterController {
     }
 
     @GetMapping("/{id}")
-    @UserAuth
+    @DkuAuth
     public ResponseEntity<EventLetterResponse> findLetter(JwtAuthentication auth,
                                                           @PathVariable("id") final Long letterId) {
         EventLetterResponse response = eventLetterService.findLetter(auth.getUserId(), letterId);
@@ -48,14 +47,14 @@ public class EventLetterController {
     }
 
     @PostMapping("/{letterId}/reports")
-    @UserAuth
+    @DkuAuth
     public ResponseEntity<Void> reportLetter(JwtAuthentication auth, @PathVariable("letterId") final Long letterId) {
         eventLetterService.reportLetter(letterId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @GetMapping("/user")
-    @UserAuth
+    @DkuAuth
     public ResponseEntity<List<EventLetterResponse>> findLettersByUser(JwtAuthentication auth) {
         List<EventLetterResponse> responses = eventLetterService.findLettersByUser(auth.getUserId());
         return ResponseEntity.ok().body(responses);
