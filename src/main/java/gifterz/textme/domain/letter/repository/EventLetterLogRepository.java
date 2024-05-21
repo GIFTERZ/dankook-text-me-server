8d0f1bc92dafd14c1249bb825121f155b1ad3b13
juy4556 <wnstjrtmdgus@gmail.com>
2024-05-21 21:51:45 +0900

    refactor: EventLetterLogRepository 내 countByUser 제거

--- src/main/java/gifterz/textme/domain/letter/repository/EventLetterLogRepository.java
+++ src/main/java/gifterz/textme/domain/letter/repository/EventLetterLogRepository.java
@@ -3,7 +3,6 @@ package gifterz.textme.domain.letter.repository;
 import gifterz.textme.domain.letter.entity.EventLetterLog;
 import gifterz.textme.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -11,8 +10,6 @@ import java.util.List;
 
 @Transactional(readOnly = true)
 public interface EventLetterLogRepository extends JpaRepository<EventLetterLog, Long> {
-
-    long countByUser(User user);
 
     List<EventLetterLog> findAllByUserId(Long userId);
 
