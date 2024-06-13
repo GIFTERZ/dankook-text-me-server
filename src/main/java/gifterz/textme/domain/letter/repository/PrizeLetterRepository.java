4211eb35c22c10ede8672559fefb83bde1999eb7
juy4556 <wnstjrtmdgus@gmail.com>
2024-06-13 14:23:40 +0900

    feature: PrizeLetterRepository DAO 추가

---
+++ src/main/java/gifterz/textme/domain/letter/repository/PrizeLetterRepository.java
@@ -0,0 +1,8 @@
+package gifterz.textme.domain.letter.repository;
+
+import gifterz.textme.domain.letter.entity.PrizeLetter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrizeLetterRepository extends JpaRepository<PrizeLetter, Long> {
+
+}
