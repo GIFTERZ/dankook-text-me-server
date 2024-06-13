40ce37a10800949e02d7744c610c3ff7da4c0b69
juy4556 <wnstjrtmdgus@gmail.com>
2024-06-13 14:13:33 +0900

    feature: PrizeLetterVO 추가

---
+++ src/main/java/gifterz/textme/domain/letter/entity/PrizeLetterVO.java
@@ -0,0 +1,27 @@
+package gifterz.textme.domain.letter.entity;
+
+import lombok.Getter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+public class PrizeLetterVO {
+    private String contents;
+    private MultipartFile webInfoImage;
+    private MultipartFile paymentImage;
+    private String cardImageUrl;
+    private Category category;
+
+    private PrizeLetterVO(String contents, MultipartFile webInfoImage, MultipartFile paymentImage,
+                          String cardImageUrl, Category category) {
+        this.contents = contents;
+        this.webInfoImage = webInfoImage;
+        this.paymentImage = paymentImage;
+        this.cardImageUrl = cardImageUrl;
+        this.category = category;
+    }
+
+    public static PrizeLetterVO of(String contents, MultipartFile image1, MultipartFile image2,
+                                   String cardImageUrl, Category category) {
+        return new PrizeLetterVO(contents, image1, image2, cardImageUrl, category);
+    }
+}
