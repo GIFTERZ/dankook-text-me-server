70976479f979c3f9a82796a80442c6c6308cc9dc
juy4556 <wnstjrtmdgus@gmail.com>
2024-06-17 07:18:21 +0900

    refactor: PrizeLetterVO에 phoneNumber 추가

--- src/main/java/gifterz/textme/domain/letter/entity/PrizeLetterVO.java
+++ src/main/java/gifterz/textme/domain/letter/entity/PrizeLetterVO.java
@@ -10,18 +10,20 @@ public class PrizeLetterVO {
     private MultipartFile paymentImage;
     private String cardImageUrl;
     private Category category;
+    private String phoneNumber;
 
     private PrizeLetterVO(String contents, MultipartFile webInfoImage, MultipartFile paymentImage,
-                          String cardImageUrl, Category category) {
+                          String cardImageUrl, Category category, String phoneNumber) {
         this.contents = contents;
         this.webInfoImage = webInfoImage;
         this.paymentImage = paymentImage;
         this.cardImageUrl = cardImageUrl;
         this.category = category;
+        this.phoneNumber = phoneNumber;
     }
 
     public static PrizeLetterVO of(String contents, MultipartFile webInfoImage, MultipartFile paymentImage,
-                                   String cardImageUrl, Category category) {
-        return new PrizeLetterVO(contents, webInfoImage, paymentImage, cardImageUrl, category);
+                                   String cardImageUrl, Category category, String phoneNumber) {
+        return new PrizeLetterVO(contents, webInfoImage, paymentImage, cardImageUrl, category, phoneNumber);
     }
 }
